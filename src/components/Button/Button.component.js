abf004275b3e59121d11f108ba0c786f4b551f9a
mre-dev <mre.dev01@gmail.com>
2022-02-01 22:51:59 +0330

    Create Button Component And Style it

--- src/components/Button/Button.component.js
+++ src/components/Button/Button.component.js
@@ -4,10 +4,29 @@ import Styles from "assets/styles/components/Button/Button.module.css";
 
 export const Button = (props) => {
     return (
-        <></>
+        <button className=
+        {`
+            ${Styles.btn}
+            ${Styles[`btn__color__${props.type}`]}
+            ${Styles[`btn__size__${props.size}`]}
+            ${props.borderRadius ?  Styles.btn__borderRadius : ''}
+        `}
+        onClick={props.click}>
+
+        {props.leftIcon}
+        {props.text}
+        {props.righIcon}
+
+        </button>
     );
 };
 
 Button.propTypes = {
-
+    text: PropTypes.string.isRequired,
+    click: PropTypes.func,
+    type: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark' ]).isRequired,
+    size: PropTypes.oneOf(['small', 'normal', 'large', 'xlarge', 'xxlarge' ]).isRequired,
+    borderRadius: PropTypes.bool,
+    leftIcon: PropTypes.element,
+    righIcon: PropTypes.element
 };
