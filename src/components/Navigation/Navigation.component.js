bbee98262ea90469266b7009eae49431dbab873f
mre-dev <mre.dev01@gmail.com>
2022-02-02 01:50:24 +0330

    Create Navigation Component ( internal or external link ) And Style it

--- src/components/Navigation/Navigation.component.js
+++ src/components/Navigation/Navigation.component.js
@@ -1,13 +1,38 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import Styles from "assets/styles/components/Navigation/Navigation.module.css";
 
 export const Navigation = (props) => {
     return (
-        <></>
+        <Fragment>
+                {
+                    props.internal ?
+                        props.active == true ?
+                            <Link id={props.id} className={props.bordered ? `${Styles[`borderedLink`]} ${Styles[`active`]}` : `${Styles[`active`]}`} to={props.link} onClick={props.click}>{props.text}</Link> : 
+                            <Link id={props.id} className={props.bordered ? `${Styles[`borderedLink`]}` : ''} to={props.link} onClick={props.click}>{props.text}</Link> : 
+                    props.external ?
+                    <a id={props.id} className={props.bordered ? `${Styles[`borderedLink`]}` : ''} href={props.link} target="_blank" onClick={props.click}>{props.text}</a> :
+                    <Link id={props.id} className={props.bordered ? `${Styles[`borderedLink`]}` : ''} to={props.link} onClick={props.click}>{props.text}</Link>
+                }
+            </Fragment>
     );
 };
 
 Navigation.propTypes = {
-    
+    id: PropTypes.string,
+    link: PropTypes.string.isRequired,
+    text: PropTypes.any.isRequired,
+    click: PropTypes.func,
+    active: PropTypes.bool,
+    internal: (props, internal, Navigation) => {
+        if (!props.internal && !props.external) {
+            return new Error(`One of props 'internal' or 'external' is Required in '${Navigation}'.`);
+        }
+    },
+    external: (props, external, Navigation) => {
+        if (!props.internal && !props.external) {
+            return new Error(`One of props 'internal' or 'external' is Required in '${Navigation}'.`);
+        }
+    }
 };
