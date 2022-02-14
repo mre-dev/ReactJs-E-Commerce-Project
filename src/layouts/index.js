a2ceb744521d2a4f2c7a70a7e283a006276753ef
mre-dev <mre.dev01@gmail.com>
2022-02-14 14:36:58 +0330

    Update Layouts Name

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,3 @@
-export { Footer } from './Footer/footer.layout.js';
+export { Footer } from './footer/footer.layout.js';
 export { Header } from './header/header.layout.js';
 export { Sidebar } from './sidebar/sidebar.layout.js';
