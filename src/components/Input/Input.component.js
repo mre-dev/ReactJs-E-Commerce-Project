fa1cf370751940754ab7a5596d96f862284a4c21
mre-dev <mre.dev01@gmail.com>
2022-02-14 14:32:19 +0330

    Create Input Component

--- src/components/Input/Input.component.js
+++ src/components/Input/Input.component.js
@@ -4,10 +4,25 @@ import Styles from "assets/styles/components/Input/Input.module.css";
 
 export const Input = (props) => {
     return (
-        <></>
+        <input
+            value={props.value}
+            onChange={props.onChange}
+            className={Styles.input}
+            type={props.type}
+            placeholder={props.placeholder}
+            name={props.name}
+            id={props.id}
+            required={props.required}
+        />
     );
 };
 
 Input.propTypes = {
-    
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    type: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    name: PropTypes.string,
+    id: PropTypes.string.isRequired,
+    required: PropTypes.bool
 };
