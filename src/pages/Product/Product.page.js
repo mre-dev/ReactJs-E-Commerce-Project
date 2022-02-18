fe6142ad8278156683904cac762d7309af444723
mre-dev <mre.dev01@gmail.com>
2022-02-19 00:29:32 +0330

    Create Product And Products Page

--- src/pages/Product/Product.page.js
+++ src/pages/Product/Product.page.js
@@ -1,4 +1,6 @@
+import { Header } from 'layouts';
 import React, { Fragment, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 import { useParams, useNavigate, Link  } from 'react-router-dom';
 import swal from 'sweetalert';
 
@@ -6,20 +8,29 @@ export const ProductPage = (props) => {
     const params = useParams();
     const navigate = useNavigate();
     const productId = params.productId;
-
+    const productName = "محصول شماره " + productId;
 
     useEffect(() => {
         if(!productId) {
-            swal('Error', 'Product not found', 'error');
+            swal('خطا', 'محصول مورد نظر یافت نشد', 'error');
         }
     }, [])
 
     return (
         <div>
+
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>{productName} | {process.env.REACT_APP_WEBSITE_NAME}</title>
+                <meta name="description" content={`${productName} %REACT_APP_WEBSITE_NAME%`} />
+            </Helmet>
+
+            <Header/>
+
             {productId ?
                 // if productId is found, render product page
                 <Fragment>
-                    <h1>Product Page {productId}</h1>
+                    <h1>Product {productId}</h1>
                 </Fragment>
                 :
                 // if productId is not found, render 404 page
--- src/pages/Products/Products.page.js
+++ src/pages/Products/Products.page.js
@@ -1,20 +1,36 @@
-import React, { useEffect } from 'react';
+import { Header } from 'layouts';
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
 import { useParams, useNavigate  } from 'react-router-dom';
 import swal from 'sweetalert';
 
 export const ProductsPage = (props) => {
     const params = useParams();
-    const navigate = useNavigate();
+    //const navigate = useNavigate();
 
-    const categoryName = params.categoryName || 'all';
-    const pageNumber = params.pageNumber || 1;
+    const [categoryName, setCategoryName] = useState(params.categoryName || 'all');
+    const [pageNumber, setPageNumber] = useState(params.pageNumber || 1);
 
     useEffect(() => {
-        navigate(`/products/${categoryName}/${pageNumber}`);
+        window.history.replaceState(null, `دسته بندی محصولات ${categoryName}`, `/products/${categoryName}/${pageNumber}`);
+        //navigate(`/products/${categoryName}/${pageNumber}`);
     }, [])
+
+    useEffect(() => {
+        setCategoryName(params.categoryName || 'all');
+        setPageNumber(params.pageNumber || 1);
+    }, [params.categoryName])
 
     return (
         <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>{categoryName} | {process.env.REACT_APP_WEBSITE_NAME}</title>
+                <meta name="description" content={`${categoryName} %REACT_APP_WEBSITE_NAME%`} />
+            </Helmet>
+
+            <Header/>
+
             Products Page <br/>
             { categoryName } <br/>
             { pageNumber }
