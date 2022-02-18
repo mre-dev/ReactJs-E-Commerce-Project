fb95a29678fba2ff172333a4ea8d55f94f237d22
mre-dev <mre.dev01@gmail.com>
2022-02-19 00:22:47 +0330

    Create Logout structure

--- src/pages/Dahsboard/Exit/Exit.page.js
+++ src/pages/Dahsboard/Exit/Exit.page.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setUserDataWhenLogin } from 'redux/type';
 
 export const UserExitPage = (props) => {
+
+    const customDispatch = useDispatch();
+    const Navigate = useNavigate();
+
+    useEffect(() => {
+        localStorage.removeItem('userData');
+        localStorage.removeItem('ACCESS_TOKEN');
+        localStorage.removeItem('REFRESH_TOKEN');
+        localStorage.removeItem('IS_LOGGED_IN');
+        customDispatch(setUserDataWhenLogin());
+        Navigate('/');
+    }, []);
+
     return (
         <div>
             Exit Page
