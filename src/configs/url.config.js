ab532dc4f1dac42632e1cca3d3dd42cca27cf4bb
mre-dev <mre.dev01@gmail.com>
2022-02-21 16:00:51 +0330

    Add GET_ORDERS Url

--- src/configs/url.config.js
+++ src/configs/url.config.js
@@ -3,3 +3,4 @@ export const REFRESH_TOKEN_URL = '/auth/refresh-token';
 export const REGISTER = '/users';
 export const GET_PRODUCTS = '/products';
 export const GET_CATEGORIES = '/category';
+export const GET_ORDERS = '/orders';
