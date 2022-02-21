9dd5c261b4423a16cee03ae5ac97f5d73b7ba5d7
mre-dev <mre.dev01@gmail.com>
2022-02-21 16:17:29 +0330

    Fix Exported Some Page Names

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,3 +1,3 @@
-export { PaymentResultFailPage } from './PaymentResultFail/PaymentResultFail.page';
-export { PaymentResultSuccessPage } from './PaymentResultSuccess/PaymentResultSuccess.page';
+export { PaymentResultFailPage } from './PaymentResultFail/paymentResultFail.page';
+export { PaymentResultSuccessPage } from './PaymentResultSuccess/paymentResultSuccess.page';
 export { PaymentPage } from './Payment/Payment.page';
