b6f2a3ca3b036bc5b7e5b2c28cac35ed06a58218
mre-dev <mre.dev01@gmail.com>
2022-02-25 02:05:52 +0330

    Add UPLOAD_IMAGE Url

--- src/configs/url.config.js
+++ src/configs/url.config.js
@@ -4,3 +4,4 @@ export const REGISTER = '/users';
 export const GET_PRODUCTS = '/products';
 export const GET_CATEGORIES = '/category';
 export const GET_ORDERS = '/orders';
+export const UPLOAD_IMAGE = '/upload';
