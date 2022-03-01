5cee1ed98e3c9e3f1baf34cd09849789a76bf160
mre-dev <mre.dev01@gmail.com>
2022-03-01 18:40:39 +0330

    Add Export SwiperSlider Component

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { OrderModal } from "./OrderModal/OrderModal.component";
 export { ProductCard } from "./ProductCard/ProductCard.component";
 export { ProductModal } from "./ProductModal/ProductModal.component";
 export { Table } from "./Table/Table.component";
+export { SwiperSlider } from "./SwiperSlider/SwiperSlider.component";
