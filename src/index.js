643d9e9f03c51198999703f385fbd11e9514fe62
mre-dev <mre.dev01@gmail.com>
2022-03-05 05:56:31 +0330

    Add ToastProvider for Show Toasts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/public.css';
-
 import { App } from 'App';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import { ToastProvider } from 'react-toast-notifications';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App/>
+            <ToastProvider>
+                <App/>
+            </ToastProvider>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
