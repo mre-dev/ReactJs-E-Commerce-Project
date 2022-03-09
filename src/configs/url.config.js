d43fe3c123de978109ed7d2566f05def9b18ea47
mre-dev <mre.dev01@gmail.com>
2022-03-09 12:05:22 +0330

    Add Product URL

--- src/configs/url.config.js
+++ src/configs/url.config.js
@@ -2,6 +2,7 @@ export const LOGIN = '/auth/login';
 export const REFRESH_TOKEN_URL = '/auth/refresh-token';
 export const REGISTER = '/users';
 export const GET_PRODUCTS = '/products';
+export const GET_PRODUCT = '/product';
 export const GET_CATEGORIES = '/category';
 export const GET_ORDERS = '/orders';
 export const UPLOAD_IMAGE = '/upload';
