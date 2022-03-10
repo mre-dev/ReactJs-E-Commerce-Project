d6d27352523201466e4885e898db79a2595d3f04
mre-dev <mre.dev01@gmail.com>
2022-03-10 08:43:18 +0330

    Create addOrder And deleteOrder Functions

--- src/api/updateOrder.js
+++ src/api/updateOrder.js
@@ -1,9 +1,27 @@
 import { GET_ORDERS } from "configs/url.config";
 import http from "services/http.service";
 
+export async function addOrder(data) {
+    try {
+        const response = await http.post(GET_ORDERS, data);
+        return response;
+    } catch (e) {
+        return Promise.reject(e);
+    }
+}
+
 export async function updateOrder(orderId, data) {
     try {
         const response = await http.put(`${GET_ORDERS}/${orderId}`, data);
+        return response;
+    } catch (e) {
+        return Promise.reject(e);
+    }
+}
+
+export async function deleteOrder(orderId) {
+    try {
+        const response = await http.delete(`${GET_ORDERS}/${orderId}`);
         return response;
     } catch (e) {
         return Promise.reject(e);
