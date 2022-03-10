76f8e3a4ac9f234fee2de2bc924eeb98ff0f10f4
mre-dev <mre.dev01@gmail.com>
2022-03-10 08:42:18 +0330

    Create Not Found Page

--- src/pages/NotFound/NotFound.page.js
+++ src/pages/NotFound/NotFound.page.js
@@ -1,11 +1,22 @@
-import { Footer, Header } from 'layouts';
 import React from 'react';
+import Styles from './NotFoundStyle.module.css';
+import { AiOutlineQuestionCircle } from 'assets/images/icons'
+import { Footer, Header } from 'layouts';
+import { Navigation } from 'components';
 
 export const NotFoundPage = (props) => {
     return (
         <div className="contentWithHeaderAndFooter">
             <Header />
-                <p>Page Not Fount</p>
+                <div className={Styles.mainbox}>
+                    <div className={Styles.err}>4</div>
+                    <AiOutlineQuestionCircle />
+                    <div className={Styles.err2}>4</div>
+                </div>
+                <div className={Styles.text}>
+                    <p>صفحه مورد نظر یافت نشد</p>
+                    <Navigation text={'بازگشت به صفحه اصلی'} link="/" intenal/>
+                </div>
             <Footer />
         </div>
     );
